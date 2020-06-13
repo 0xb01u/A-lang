2f134c546ca3b916dad4a5651231e56534989c9d
mandeca <manuel.castro@alumnos.uva.es>
2020-06-13 20:22:07 +0200

    Commit final?

---
+++ GrupoG/src/symtab.h
@@ -0,0 +1,19 @@
+/**
+ * Compilador de A - Proyecto final GLF
+ * Manuel de Castro Caballero, María Ruiz Molina, Andrés Trigueros Vega
+ * Curso 2019-2020
+ */
+
+/* Tipo "símbolo" para la tabla de símbolos 
+   (Par tipo de dato - valor del dato) */
+typedef struct symbol_s {
+	int type;
+	union {
+		double real;
+		long integer;
+	} value;
+} symbol;
+
+/* Declaración de funciones de symtab.c */
+symbol get(char *id);
+void edit(char *id, symbol value);
